cf27e1e24566a5a0328b4ca3a9c33d493979f00f
Laughing6901 <vandat96tn@gmail.com>
2021-07-23 01:16:12 +0700

    add passport fb token

--- models/users.js
+++ models/users.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    
     username: {
       type: String,
         default: ''
